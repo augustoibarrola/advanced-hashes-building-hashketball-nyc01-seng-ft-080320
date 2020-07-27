f64a67e99be801c0b76f292ffa8600c440c731f5
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-27 22:05:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,12 +3,31 @@ def game_hash
     home: {
       team_name: "",
       colors: Array.new,
-      players: Array.new
+      players: [
+        {player_name: ""},
+        {number: ""},
+        {shoe: ""},
+        {points: ""},
+        {rebounds: ""},
+        {assists: ""},
+        {steals: ""},
+        {blocks: ""},
+        {slam_dunks: ""}
     },
     away: {
       team_name: "",
       colors: Array.new,
-      players: Array.new
+      players: [
+        {player_name: ""},
+        {number: ""},
+        {shoe: ""},
+        {points: ""},
+        {rebounds: ""},
+        {assists: ""},
+        {steals: ""},
+        {blocks: ""},
+        {slam_dunks: ""}
+      ]
     }
   }
 end
